ce697533d7dc001ff21d340e4fc9b2de499a9e04
dam <dam@203P17>
2023-02-17 09:53:34 +0100

    Pruebas de examen
    esta la tag ALfa metida, la nueva Rama Produccion,
    la nueva clase con sus Souts y la nueva etiqueta Beta

---
+++ src/cvsamayuelas/Clase2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cvsamayuelas;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase2 {
+    public static void main(String[] args) {
+        System.out.println("Estoy en la rama PRODUCCION");
+        System.out.println("Hola2");
+    }
+}
